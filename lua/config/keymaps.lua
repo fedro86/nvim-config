6dafc63912301381b0d03bf38b9c81a6dc56e1ef
fedro <federico.conticello@hydroscan.be>
2025-01-03 16:42:27 +0100

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- motion keys (left, down, up, right)
+vim.keymap.set({'n', 'v'}, 'j', 'h')
+vim.keymap.set({'n', 'v'}, ',', 'j')
+vim.keymap.set({'n', 'v'}, 'k', 'k')
+vim.keymap.set({'n', 'v'}, 'l', 'l')
+
+
+-- beginning/end of word
+vim.keymap.set({'n', 'v'}, 'h', 'b')
+vim.keymap.set({'n', 'v'}, 'ò', 'e')
+
+-- repeat
+vim.keymap.set({'n', 'v'}, '\'', ';')
---
+++ lua/config/plugin_config/init.lua
@@ -0,0 +1,3 @@
+require("config.plugin_config.gruvbox")
+require("config.plugin_config.lualine")
+require("config.plugin_config.nvim-tree")
